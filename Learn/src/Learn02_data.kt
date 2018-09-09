9f8f4890af0e48a0f81be64401ca75146c73b776
xiongliqiang <1009224322@qq.com>
2018-09-09 14:31:21 +0800

    hello&data

---
+++ Learn/src/Learn02_data.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+
+    // 布尔值
+    val mBoolean: Boolean = true
+    val mBoolean2: Boolean = false
+
+    val a: Int = 0xff
+    val aFloat:Float = 2.0f
+    val aDouble:Double = 2.0
+    println(Int.MAX_VALUE)
+
+    val char:Char = '\n'
+    println(char)
+
+}
+
